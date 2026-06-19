56486376b5f39ef62357b62a23f00a8996bb5d48
Sean Rankin <sean.rankin@gmail.com>
2026-06-19 09:32:38 -0400

    Fix Microsoft Outlook link fetch issue

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,11 +4,21 @@ import { createClient } from "@/lib/supabase/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
+  const tokenHash = searchParams.get("token_hash");
+  const type = searchParams.get("type");
+
+  const supabase = await createClient();
 
   if (code) {
-    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-
+    if (!error) {
+      return NextResponse.redirect(origin);
+    }
+  } else if (tokenHash && type) {
+    const { error } = await supabase.auth.verifyOtp({
+      token_hash: tokenHash,
+      type: type as "email" | "sms",
+    });
     if (!error) {
       return NextResponse.redirect(origin);
     }
